55be1fb4c6492529735b050747411d38f8eba4a8
Nhan <nhtrnguy@ucsc.edu>
2023-10-10 21:56:08 -0700

    step 1 complete

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,12 @@ const gameName = "Nhan's Game";
 
 document.title = gameName;
 
+const mainbt: HTMLButtonElement = document.createElement("button");
+mainbt.className = "mainButton";
+mainbt.type = "button";
+mainbt.textContent = "click the Christmas Tree 🎄";
+
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
+app.append(mainbt);
