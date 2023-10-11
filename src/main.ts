652ad8b77292e12a0ddb84e5478cb9793d10b8bc
Nhan <nhtrnguy@ucsc.edu>
2023-10-10 22:26:11 -0700

    step 2 complete

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import "./style.css";
 
 const app: HTMLDivElement = document.querySelector("#app")!;
+const counter: HTMLDivElement = document.querySelector("#counter")!;
+
 
 const gameName = "Nhan's Game";
+
+let cnt: number = 0;
 
 document.title = gameName;
 
@@ -11,7 +15,15 @@ mainbt.className = "mainButton";
 mainbt.type = "button";
 mainbt.textContent = "click the Christmas Tree 🎄";
 
+let countdisplay = document.createElement("h2");
+countdisplay.innerHTML = "Number of christmas has passed: 0"
+mainbt.addEventListener("click", function () {
+  cnt++;
+  countdisplay.innerHTML = "Number of christmas has passed: " + cnt.toString();
+});
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
 app.append(mainbt);
+counter.append(countdisplay);
+
