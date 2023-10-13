2856e5db7e224fd3b1af9d704a221398f2ddf565
Nhan <nhtrnguy@ucsc.edu>
2023-10-12 20:11:43 -0700

    step 10 complete

--- src/main.ts
+++ src/main.ts
@@ -20,21 +20,35 @@ const availableItems: Item[] = [
     cost: 10,
     rate: 0.1,
     upgradecnt: 0,
-    description: "An ornament you buy at a dollar tree"
+    description: "An ornament you buy at a dollar tree",
   },
   {
     name: "Super Ornament",
     cost: 100,
     rate: 2,
     upgradecnt: 0,
-    description: "An ornament you buy at supermarkets"
+    description: "An ornament you buy at supermarkets",
   },
   {
     name: "Ultra Ornament",
     cost: 1000,
     rate: 50,
     upgradecnt: 0,
-    description: "An ornament you buy at specialized christmas stores"
+    description: "An ornament you buy at specialized christmas stores",
+  },
+  {
+    name: "Santa Figurine",
+    cost: 3000,
+    rate: 100,
+    upgradecnt: 0,
+    description: "A special item you can only buy from Santa himself",
+  },
+  {
+    name: "Christmas Tree Sap",
+    cost: 5000,
+    rate: 500,
+    upgradecnt: 0,
+    description: "An item collected only from the alpha Christmas Tree",
   },
 ];
 
@@ -45,62 +59,41 @@ document.title = gameName;
 
 //button names
 const mainbt: HTMLButtonElement = document.createElement("button");
-const growbt: HTMLButtonElement = document.createElement("button");
-const growbt2: HTMLButtonElement = document.createElement("button");
-const growbt3: HTMLButtonElement = document.createElement("button");
+const growbts: HTMLButtonElement[] = [
+  document.createElement("button"),
+  document.createElement("button"),
+  document.createElement("button"),
+  document.createElement("button"),
+  document.createElement("button"),
+];
 
 mainbt.className = "mainButton";
 mainbt.type = "button";
-mainbt.textContent = "click the Christmas Tree 🎄";
+mainbt.innerHTML = `click the Christmas Tree 🎄`;
 
-growbt.className = "growButton";
-growbt.type = "button";
-growbt.textContent =
-  availableItems[0].name +
-  " (" +
-  availableItems[0].cost +
-  " Christmas): " +
-  "+" +
-  availableItems[0].rate +
-  " Christmas/Year";
-growbt.disabled = true;
-
-growbt2.className = "growButton2";
-growbt2.type = "button";
-growbt2.textContent =
-  availableItems[1].name +
-  " (" +
-  availableItems[1].cost +
-  " Christmas): " +
-  "+" +
-  availableItems[1].rate +
-  " Christmas/Year";
-growbt2.disabled = true;
-
-growbt3.className = "growButton";
-growbt3.type = "button";
-growbt3.textContent =
-  availableItems[2].name +
-  " (" +
-  availableItems[2].cost +
-  " Christmas): " +
-  "+" +
-  availableItems[2].rate +
-  " Christmas/Year";
-growbt3.disabled = true;
+for (let i = 0; i < 5; i++) {
+  growbts[i].className = "growButton";
+  growbts[i].type = "button";
+  growbts[i].innerHTML = messageGen(availableItems[i]);
+  growbts[i].disabled = true;
+}
 
 //Texts setups
 let countdisplay = document.createElement("h2");
 let countrate = document.createElement("h2.1");
-let upgrade1 = document.createElement("h2.2");
-let upgrade2 = document.createElement("h2.2");
-let upgrade3 = document.createElement("h2.2");
+const upgrades: HTMLElement[] = [
+  document.createElement("h2.2"),
+  document.createElement("h2.2"),
+  document.createElement("h2.2"),
+  document.createElement("h2.2"),
+  document.createElement("h2.2"),
+];
 
-countdisplay.innerHTML = "Number of christmas has passed: 0";
+countdisplay.innerHTML = `Number of christmas has passed: 0`;
 countrate.innerHTML = "Christmas pass rate: 0 Christmas/Year";
-upgrade1.innerHTML = "Regular Ornament purchased: 0";
-upgrade2.innerHTML = "Super Ornament purchased: 0";
-upgrade3.innerHTML = "Ultra Ornament purchased: 0";
+for (let i = 0; i < 5; i++) {
+  upgrades[i].innerHTML = availableItems[i].name + " purchased: 0";
+}
 
 //button listeners
 mainbt.addEventListener("click", function () {
@@ -109,62 +102,21 @@ mainbt.addEventListener("click", function () {
     "Number of christmas has passed: " + Math.round(cnt).toString();
 });
 
-growbt.addEventListener("click", function () {
-  cnt -= availableItems[0].cost;
-  growth += 0.1;
-  availableItems[0].upgradecnt++;
-  availableItems[0].cost *= 1.15;
-  growbt.textContent =
-    availableItems[0].name +
-    " (" +
-    availableItems[0].cost.toFixed(2) +
-    " Christmas): " +
-    "+" +
-    availableItems[0].rate +
-    " Christmas/Year";
-  countrate.innerHTML =
-    "Christmas pass rate: " + growth.toFixed(1) + " Christmas/Year";
-  upgrade1.innerHTML =
-    "Regular Ornament purchased: " + availableItems[0].upgradecnt.toString();
-});
-
-growbt2.addEventListener("click", function () {
-  cnt -= availableItems[1].cost;
-  growth += 2;
-  availableItems[1].upgradecnt++;
-  availableItems[1].cost *= 1.15;
-  growbt2.textContent =
-    availableItems[1].name +
-    " (" +
-    availableItems[1].cost.toFixed(2) +
-    " Christmas): " +
-    "+" +
-    availableItems[1].rate +
-    " Christmas/Year";
-  countrate.innerHTML =
-    "Christmas pass rate: " + growth.toFixed(1) + " Christmas/Year";
-  upgrade2.innerHTML =
-    "Super Ornament purchased: " + availableItems[1].upgradecnt.toString();
-});
-
-growbt3.addEventListener("click", function () {
-  cnt -= availableItems[2].cost;
-  growth += 50;
-  availableItems[2].upgradecnt++;
-  availableItems[2].cost *= 1.15;
-  growbt3.textContent =
-    availableItems[2].name +
-    " (" +
-    availableItems[2].cost.toFixed(2) +
-    " Christmas): " +
-    "+" +
-    availableItems[2].rate +
-    " Christmas/Year";
-  countrate.innerHTML =
-    "Christmas pass rate: " + growth.toFixed(1) + " Christmas/Year";
-  upgrade3.innerHTML =
-    "Ultra Ornament purchased: " + availableItems[2].upgradecnt.toString();
-});
+for (let i = 0; i < 5; i++) {
+  growbts[i].addEventListener("click", function () {
+    cnt -= availableItems[i].cost;
+    growth += availableItems[i].rate;
+    availableItems[i].upgradecnt++;
+    availableItems[i].cost *= 1.15;
+    growbts[i].innerHTML = messageGen(availableItems[i]);
+    countrate.innerHTML =
+      "Christmas pass rate: " + growth.toFixed(1) + " Christmas/Year";
+    upgrades[i].innerHTML =
+      availableItems[i].name +
+      " purchased: " +
+      availableItems[i].upgradecnt.toString();
+  });
+}
 
 //Update runners
 requestAnimationFrame(adder);
@@ -175,20 +127,17 @@ const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
 app.append(mainbt);
-app.append(growbt);
-app.append(growbt2);
-app.append(growbt3);
+for (let i = 0; i < 5; i++) {
+  app.append(growbts[i]);
+}
 counter.append(countdisplay);
 counter.append(countrate);
 let linebr = document.createElement("br");
-counter.appendChild(linebr);
-counter.append(upgrade1);
-linebr = document.createElement("br");
-counter.appendChild(linebr);
-counter.append(upgrade2);
-linebr = document.createElement("br");
-counter.appendChild(linebr);
-counter.append(upgrade3);
+for (let i = 0; i < 5; i++) {
+  linebr = document.createElement("br");
+  counter.appendChild(linebr);
+  counter.append(upgrades[i]);
+}
 
 //add to the value
 function adder(Millis: number) {
@@ -202,20 +151,20 @@ function adder(Millis: number) {
 
 //Checking buttons enabling
 function checker() {
-  if (cnt >= availableItems[0].cost) {
-    growbt.disabled = false;
-  } else {
-    growbt.disabled = true;
-  }
-  if (cnt >= availableItems[1].cost) {
-    growbt2.disabled = false;
-  } else {
-    growbt2.disabled = true;
-  }
-  if (cnt >= availableItems[2].cost) {
-    growbt3.disabled = false;
-  } else {
-    growbt3.disabled = true;
+  for (let i = 0; i < 5; i++) {
+    growbts[i].disabled = cnt < availableItems[i].cost;
   }
   requestAnimationFrame(checker);
+}
+
+function messageGen(Item: Item) {
+  return (
+    Item.name +
+    `<br> (` +
+    Item.cost.toFixed(2) +
+    ` Christmas): +` +
+    Item.rate +
+    ` Christmas/Year <br>` +
+    Item.description
+  );
 }
